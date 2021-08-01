15d4d1cf1051fd15e0b7089bf1838954d1f911f1
Valter Schütz <valterschutz@protonmail.com>
2021-08-01 15:07:05 +0200

    Add movement functionality

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const boxSize = 50
 const nrOfBoxes = 10  // For one direction only, so grid contains nrOfBoxes^2 boxes
 
+// The snake, represented as an array
+let snake = [11, 12, 13]
+// Array containing boxes, used for easier referencing
+let boxes = []
+// Direction. 1 = right, -1 = left, 10 = down, -10 = up
+let direction = 1
+
 // Elements
 const grid = document.getElementById("grid")
 
@@ -14,4 +21,25 @@ for (let i = 0; i < nrOfBoxes * nrOfBoxes; i++) {
     box.style.width = `${boxSize}px`
     box.style.height = `${boxSize}px`
     grid.appendChild(box)
+    boxes.push(box)
+}
+
+drawSnake()
+
+
+
+// Functions
+function drawSnake() {
+    snake.forEach(index => { boxes[index].classList.add("snake") })
+}
+
+function undrawSnake() {
+    snake.forEach(index => { boxes[index].classList.remove("snake") })
+}
+
+function moveSnake() {
+    undrawSnake()
+    snake.push(snake[snake.length - 1] + direction)
+    snake.shift()
+    drawSnake()
 }
